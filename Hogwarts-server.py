d85d1d663b4d5be63e8497907be7728ac8ef406d
Danielle-Tal <danielle.tal1986@gmail.com>
2020-01-11 14:11:51 +0200

    MVP for server side

---
+++ Hogwarts-server.py
@@ -0,0 +1,66 @@
+from flask import Flask, request
+from students import StudentsData, Student
+from json import JSONEncoder
+from flask import jsonify
+app = Flask(__name__)
+
+students_data = StudentsData()
+
+
+class MyEncoder(JSONEncoder):
+        def default(self, o):
+            return o.__dict__
+
+
+@app.route('/students', methods=["GET", "POST"])
+def handle_students():
+    if request.method == "GET":
+        return get_students()
+    if request.method == "POST":
+        return new_student()
+
+def get_students():
+    return jsonify(students=list(students_data.get_students().values()))
+
+
+@app.route('/students/<studentid>', methods=["GET", "DELETE", "PUT"])
+def handle_student(studentid):
+    if request.method == "GET":
+        return get_student(studentid)
+    if request.method == "DELETE":
+        return remove_student(studentid)
+    if request.method == "PUT":
+        return update_student()
+
+
+def get_student(studentid):
+    return jsonify(students_data.get_student(studentid))
+
+
+def remove_student(studentid):
+    student = get_student(studentid)
+    students_data.remove_student(studentid)
+    return student
+
+
+def new_student():
+    req_data = request.get_json()
+    student = Student(req_data["firstname"], req_data["lastname"], req_data["existing_magic_skillz"],
+                      req_data["desired_magic_skillz"], req_data["intrested_in_course"])
+    students_data.add_student(student)
+    return get_student(student.studentid)
+
+
+def update_student():
+    req_data = request.get_json()
+    student = Student(firstname=req_data["firstname"], lastname=req_data["lastname"], studentid=req_data["studentid"],
+                      existing_magic_skillz=req_data["existing_magic_skillz"],
+                      desired_magic_skillz=req_data["desired_magic_skillz"],
+                      intrested_in_course=req_data["intrested_in_course"], creationtime=req_data["creationtime"])
+    students_data.update_student(student)
+    return get_student(student.studentid)
+
+
+if __name__ == '__main__':
+    app.json_encoder = MyEncoder
+    app.run()
---
+++ students.py
@@ -0,0 +1,51 @@
+import time
+
+class Student:
+    counter = 1
+
+    def __init__(self, firstname, lastname, existing_magic_skillz,desired_magic_skillz,
+                 intrested_in_course, creationtime=None, studentid=None):
+        if studentid is None:
+            self.studentid = str(Student.counter)
+            Student.counter += 1
+            self.creationtime = time.asctime(time.localtime(time.time()))
+            self.last_updated_time = self.creationtime
+        else:
+            self.studentid = studentid
+            self.creationtime = creationtime
+            self.last_updated_time = time.asctime(time.localtime(time.time()))
+        self.firstname = firstname
+        self.lastname = lastname
+        self.existing_magic_skillz = existing_magic_skillz
+        self.desired_magic_skillz = desired_magic_skillz
+        self.intrested_in_course = intrested_in_course
+
+
+
+class StudentsData:
+    def __init__(self):
+        print("starting...")
+        self.students = {"1": Student("Harry", "Potter", {"Healing": 4, "Poison": 2}, {"Water breathing": 3},
+                                      "Alchemy basics"),
+                         "2": Student("Ron", "Weisly", {"Illusion": 3, "Elemental": 1}, {"Invulnerability": 4},
+                                      "Magic for day-to-day life"),
+                         "3": Student("Hermaionie", "Grainger", {"Conjuror": 5, "Animation": 5, "Self-detonation": 2},
+                                      {"Disintegration":5}, "Dating with magic")}
+
+    def get_students(self):
+        return self.students
+
+    def get_student(self,studentid):
+        return self.students.get(studentid)
+
+    def add_student(self, student):
+        self.students[student.studentid] = student
+
+    def remove_student(self, studentid):
+        self.students.pop(studentid)
+
+    def update_student(self, student):
+        self.students[student.studentid] = student
+
+
+
